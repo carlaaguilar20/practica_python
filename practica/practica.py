bbaaad9cb7a4a812471d24bf030b22115d2e58c4
carlaaguilar20 <carlaaguilar20@gmail.com>
2025-05-26 09:29:59 -0300

    probando pista2

--- practica/practica.py
+++ practica/practica.py
@@ -1,19 +1,19 @@
 import random
 
 def jugar():
-    # Generar número secreto
+   
     numero_secreto = random.randint(1, 40)
     intentos = 0
     max_intentos = 5
 
-    # Mensaje de bienvenida
+  
     print("\n🎮 ADIVINA EL NÚMERO 🤯")
     print("- El número se encuentra entre 1 y 40")
     print("- Tienes cinco (5) intentos")
     print("- ¡Suerte!\n")
 
     while intentos < max_intentos:
-        # Mostrar pistas en los intentos 4 y 5
+       
         if intentos == 3:
             rango_min = max( 1, numero_secreto - 3)
             rango_max = min( 40, numero_secreto + 3)
@@ -23,7 +23,7 @@ def jugar():
             rango_max = min( 40,numero_secreto + 2)
             print(f"💡 Pista: ¡Último intento! El número está entre {rango_min} y {rango_max}.")
 
-        # Solicitar número al usuario
+     
         try:
             intento = int(input(f"Intento {intentos + 1} de {max_intentos} - Adivina el número (entre 1 y 40): "))
         except ValueError:
@@ -47,7 +47,7 @@ def jugar():
     if intento != numero_secreto:
         print(f"😢 Lo siento, has perdido. El número secreto era {numero_secreto}.")
 
-# Bucle principal para volver a jugar
+
 while True:
     jugar()
     respuesta = input("\n¿Deseas jugar otra vez? (sí/no): ").strip().lower()
